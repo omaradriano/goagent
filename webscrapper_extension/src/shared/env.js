6af172bf32a03a32fb000a23a25387feb88238b1
Omar Adrian <oadrian38@gmail.com>
2026-08-27 11:35:04 -0600

    fix: env credentials estaban apuntando a local

--- webscrapper_extension/src/shared/env.js
+++ webscrapper_extension/src/shared/env.js
@@ -1,5 +1,5 @@
-export const SERVER_URL = "http://localhost:3006";
-// export const SERVER_URL = "https://cobranzawebscrapperserver-production.up.railway.app";
+// export const SERVER_URL = "http://localhost:3006";
+export const SERVER_URL = "https://cobranzawebscrapperserver-production.up.railway.app";
 
-export const FRONTEND_URL = "http://localhost:5173";
-// export const FRONTEND_URL = "https://www.goagent.com.mx";
+// export const FRONTEND_URL = "http://localhost:5173";
+export const FRONTEND_URL = "https://www.goagent.com.mx";
